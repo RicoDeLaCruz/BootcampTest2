1bcdb57f3bdefa4b2e05ff0309df4f7b759745e9
RicoDeLaCruz <enricodelacruz995@gmail.com>
2023-01-23 21:37:39 +0800

    Compressed Script(TakeHome)

--- create.ps1
+++ create.ps1
@@ -1,38 +1,24 @@
-#-initialization
-$CreateCSV = Import-csv -path "C:\Users\EnricoDeLC\OneDrive - FUJITSU\Desktop\PowerShellBootcamp\create.csv" 
-
-#transform data
-$PurposeItem = $CreateCSV | select-object -ExpandProperty Purpose
-$ArrayMembersItem = ($CreateCSV | select -ExpandProperty Members) -split (',')
-$Item = $CreateCSV | Select -ExpandProperty Name 
-#subtransform
-$TrimedItem = ($item.TrimEnd()).TrimStart()  #Removes white spaces
-$NoCharsItem = $TrimedItem -replace '[\W]', ''  #Removes characters
-$LowerCasedItem = $NoCharsItem.ToLower()
-$AppendedItem = "GRS_" + $LowerCasedItem 
-#Assembling
-
-$AssembledObj = [PSCustomObject]@{
-    Name = $NoCharsItem
-    DisplayName = "GRS " +$NoCharsItem
-    PrimarySmtpAddress = $AppendedItem + "@solutionautdev.onmicrosoft.com"
-    Description = "`n Created at G07PHXNWES00293"+
-    "`n Created by: be.fernandez"+
-    "`n Created on: 10/18/2022 14:06:41"+
-    "`n`n=========`n" + $PurposeItem 
+$global:RootPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$json = Get-Content "$Rootpath\config.json" -Raw | ConvertFrom-Json
+$CreateCSV = Import-Csv -Path "$Rootpath\create.csv"
+Start-Transcript -Path "$Rootpath\Create_$(Get-Date -Format "MMddyyyyHHmm").txt"
+$PurposeItem = $CreateCSV | Select -ExpandProperty Purpose
+$ArrayMembersItem = ($CreateCSV | Select -ExpandProperty Members) -split (',')
+$Name = ($CreateCSV | Select -ExpandProperty Name).TrimEnd().TrimStart() -replace '[\W]','' #Combined Trimmed and No Chars
+$obj = [PSCustomObject]@{
+    Name = $Name.ToLower()
+    DisplayName = $json.DisplayNamePrefix + $Name
+    PrimarySmtpAddress = $json.AliasPrefix + $Name.ToLower() + "@" + $json.DomainName
+    Description = "`n Created at: " + $env:COMPUTERNAME + "`n Created by: " + $env:USERNAME + "`n Created on: "  + ($(Get-Date)) + "`n`n=========`n" + $PurposeItem
 }
-#Output
-
 Write-Host "`n`n Name:" -ForegroundColor Cyan
-$AssembledObj.Name
+$obj.Name
 Write-Host "`n`n DisplayName:" -ForegroundColor Cyan
-$AssembledObj.DisplayName
+$obj.DisplayName
 Write-Host "`n`n PrimarySmtpAddress:" -ForegroundColor Cyan
-$AssembledObj.PrimarySmtpAddress
+$obj.PrimarySmtpAddress
 Write-Host "`n`n Description:" -ForegroundColor Cyan
-$AssembledObj.Description
-
+$obj.Description
 Write-Host "`n`n Members:" -ForegroundColor Cyan
 $ArrayMembersItem
-
-
+Stop-Transcript
