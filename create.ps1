dbb704fd27efcf0bede1eda57805a39ae67da689
RicoDeLaCruz <enricodelacruz995@gmail.com>
2023-01-31 21:16:09 +0800

    Based on second work home task

--- create.ps1
+++ create.ps1
@@ -1,24 +1,95 @@
-$global:RootPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$json = Get-Content "$Rootpath\config.json" -Raw | ConvertFrom-Json
-$CreateCSV = Import-Csv -Path "$Rootpath\create.csv"
-Start-Transcript -Path "$Rootpath\Create_$(Get-Date -Format "MMddyyyyHHmm").txt"
-$PurposeItem = $CreateCSV | Select -ExpandProperty Purpose
-$ArrayMembersItem = ($CreateCSV | Select -ExpandProperty Members) -split (',')
-$Name = ($CreateCSV | Select -ExpandProperty Name).TrimEnd().TrimStart() -replace '[\W]','' #Combined Trimmed and No Chars
-$obj = [PSCustomObject]@{
-    Name = $Name.ToLower()
-    DisplayName = $json.DisplayNamePrefix + $Name
-    PrimarySmtpAddress = $json.AliasPrefix + $Name.ToLower() + "@" + $json.DomainName
-    Description = "`n Created at: " + $env:COMPUTERNAME + "`n Created by: " + $env:USERNAME + "`n Created on: "  + ($(Get-Date)) + "`n`n=========`n" + $PurposeItem
+try {
+    #--init
+    $global:ErrorActionPreference = "Stop"
+    $global:RootPath = split-path -parent $MyInvocation.MyCommand.Definition
+    $json = Get-Content "$RootPath\config.json" -Raw | ConvertFrom-Json 
+    $counter = 0 
+
+    
+
+    function Get-Kill {
+        param (
+            $Mode
+        )
+        if ($Mode -eq "Hard") {
+            $e = $_.Exception.GetType().FullName
+            $line = $_.InvocationInfo.ScriptLineNumber
+            $msg = $_.Exception.Message
+            Write-Output "$(Get-Date -Format "HH:mm")[Error]: Initialization failed at line [$line] due [$e] `n`nwith details `n`n[$msg]`n"
+            Write-Output "`n`n------------------END-------------------------"
+            Stop-Transcript | Out-Null
+            ClearCreateCSV
+            exit
+        }else{
+            Write-Output "`n`n------------------END-------------------------"
+            Stop-Transcript | Out-Null
+            ClearCreateCSV
+            exit
+        }
+        
+    }
+
+    function CheckCreateCSV {
+        $global:CreateCSV = Import-Csv -Path "$RootPath\create.csv"
+        
+        $missingValue = $false
+        foreach ($c in $CreateCSV){
+          if ($c.Name -eq "" -or $c.Members -eq "" -or $c.Purpose -eq "") {
+            $missingValue = $true
+            break
+          }
+        }
+      
+        if ($missingValue) {
+          Write-Output "$(Get-Date -Format "HH:mm")[Error]: Missing value in the create.csv file. Aborting."
+          Stop-Transcript | Out-Null
+          exit
+        }
+    }
+
+    function ClearCreateCSV {
+         $global:CreateCSV = Import-Csv -Path "$RootPath\create.csv"
+        $CreateCSV | ForEach-Object {
+            $_.Name = ""
+            $_.Members = ""
+            $_.Purpose = ""
+          }
+          $CreateCSV | Export-Csv -Path "$RootPath\create.csv" -NoTypeInformation   
+    }
+
+    Start-Transcript -Path "$RootPath\Create_localtime_$(Get-Date -Format "MMddyyyyHHmm").txt" | Out-Null
+    
+    Write-Output "`n`n------------------BEGIN-------------------------"
+    Write-Output "$(Get-Date -Format "HH:mm")[Log]: Initialization success"
+    CheckCreateCSV
+    foreach($c in $CreateCSV){
+        #--transform
+        Write-Output "`n$(Get-Date -Format "HH:mm")[Log]: Transforming data"
+        $NoCharsItem = $(($($c.Name).TrimEnd()).TrimStart())  -replace '[\W]', '' #remove whitespace and special chars
+        #--assembly
+        Write-Output "$(Get-Date -Format "HH:mm")[Log]: Assembling output"
+        $AssembledObj = [PSCustomObject]@{
+            Name = $NoCharsItem
+            DisplayName  = $json.DisplayNamePrefix + $NoCharsItem
+            PrimarySmtpAddress  = $($json.AliasPrefix + $($NoCharsItem.ToLower())) + "@" + $json.DomainName #join and convert to lowercases
+            Description = "`n Created at: " + $env:COMPUTERNAME + "`n Created by: " + $env:USERNAME + "`n Created on: "  + ($(Get-Date)) + "`n`n=========`n" + $c.Purpose
+            Members = ($c.Members) -split (',') #turm members to array
+        }
+        #--output
+        $counter++
+        Write-Output "`n------------------OUTPUT($counter)-------------------------"
+        foreach ($currentItemName in $(@("Name","DisplayName","PrimarySmtpAddress","Description","Members")) ) {
+            Write-Host "`n`n $($currentItemName):" -foregroundcolor Cyan
+            $AssembledObj.$($currentItemName)
+        }
+        if ($($AssembledObj.Members).length -gt 0) {
+            
+        }else{
+            Write-Host "No members found" -foregroundcolor Yellow
+        }
+    }
+    Get-Kill 
 }
-Write-Host "`n`n Name:" -ForegroundColor Cyan
-$obj.Name
-Write-Host "`n`n DisplayName:" -ForegroundColor Cyan
-$obj.DisplayName
-Write-Host "`n`n PrimarySmtpAddress:" -ForegroundColor Cyan
-$obj.PrimarySmtpAddress
-Write-Host "`n`n Description:" -ForegroundColor Cyan
-$obj.Description
-Write-Host "`n`n Members:" -ForegroundColor Cyan
-$ArrayMembersItem
-Stop-Transcript
+catch {
+    Get-Kill -Mode "Hard"
+}
